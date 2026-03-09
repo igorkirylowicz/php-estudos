9812e14b47242855ba6a59052c79a352e05a9f44
Igor <igorandrey199@gmail.com>
2026-03-09 18:56:42 -0300

    aprendendo if

--- primeiro.php
+++ primeiro.php
@@ -1,6 +1,7 @@
 
 <?php
 
+/*
 echo "Primeiro código PHP dentro da pasta estudo";
 
 ?>
@@ -13,3 +14,25 @@ $idade = 26;
 echo "Meu nome é $nome e tenho $idade anos";
 
 // teste de salvamento para entender quando salva no vscode, quando salva no Git e quando salva no GitHub
+*/
+
+
+
+// Condicionais
+
+$idade = 20;
+/*
+if ($idade >= 18) 
+    echo "Maior de idade ";
+    echo "pode entrar";
+
+*/    
+
+$idade = 17;
+
+if ($idade >= 18) {
+    echo "Maior de idade";
+} else {
+    echo "Menor de idade";
+}
+
