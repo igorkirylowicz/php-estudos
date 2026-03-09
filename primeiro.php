1784c6a74e10804f52693cf7654caa92ee821619
Igor <igorandrey199@gmail.com>
2026-03-09 18:17:29 -0300

    teste salvamento

--- primeiro.php
+++ primeiro.php
@@ -11,3 +11,5 @@ $nome = "Igor";
 $idade = 26;
 
 echo "Meu nome é $nome e tenho $idade anos";
+
+// teste de salvamento para entender quando salva no vscode, quando salva no Git e quando salva no GitHub
