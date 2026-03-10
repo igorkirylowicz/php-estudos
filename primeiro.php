f8ed2f24394b5258cfd86dcd3ac1c46140c98103
Igor <igorandrey199@gmail.com>
2026-03-09 22:37:27 -0300

    a dormir

--- primeiro.php
+++ primeiro.php
@@ -27,12 +27,16 @@ if ($idade >= 18)
     echo "pode entrar";
 
 */    
-
-$idade = 17;
+/*
+$idade = 18;
 
 if ($idade >= 18) {
     echo "Maior de idade";
 } else {
     echo "Menor de idade";
 }
+*/
+
+
+
 
